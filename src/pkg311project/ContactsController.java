8690e40b39a01ba46792b2e1d688a0bda2e68198
andylx96 <andylx96@gmail.com>
2017-05-01 16:49:11 -0400

    added import and export

--- src/pkg311project/ContactsController.java
+++ src/pkg311project/ContactsController.java
@@ -7,12 +7,14 @@ package pkg311project;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.RowFilter;
 import javax.swing.event.DocumentEvent;
@@ -23,24 +25,29 @@ import javax.swing.event.DocumentListener;
  * @author ajl5735
  */
 public class ContactsController {
+
     ContactsModel contacts_model;
     ContactsFrame contacts_frame;
     String tempC;
-    
+
     ArrayList<Contacts> contactsList = new ArrayList();
     String currentUsersName;
-    
-    ContactsController(ContactsModel contacts_model, ContactsFrame contacts_frame, String username){
+
+    ContactsController(ContactsModel contacts_model, ContactsFrame contacts_frame, String username) {
         this.contacts_model = contacts_model;
         this.contacts_frame = contacts_frame;
         setCurrentUsersName(username);
-        contacts_frame.getPanel().getContact_view().getAdd().addActionListener(new ContactsController.AddButtonListener());
-        contacts_frame.getPanel().getContact_view().getDelete().addActionListener(new ContactsController.ContactDeleteButtonListener());
+        contacts_frame.getPanel().getContact_view().getAdd().addActionListener(new AddButtonListener());
+        contacts_frame.getPanel().getContact_view().getDelete().addActionListener(new ContactDeleteButtonListener());
         updateArrayAndTable();
         contacts_frame.getPanel().getContact_view().getFilter().getDocument().addDocumentListener(new SearchDocumentListener());
-        
+        contacts_frame.getPanel().getContact_view().getImportButton().addActionListener(new ImportButtonListener());
+
+        contacts_frame.getPanel().getContact_view().getSave().addActionListener(new SaveButtonListener());
+        contacts_frame.getPanel().getContact_view().getExport().addActionListener(new ExportButtonListener());
+
     }
-    
+
     class ContactDeleteButtonListener implements ActionListener {
 
         @Override
@@ -56,14 +63,110 @@ public class ContactsController {
 
         }
     }
-    
+
+    class SaveButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            int resp = JOptionPane.showConfirmDialog(null, "Are You Sure You Want To Save All Changes?");
+            if (resp == JOptionPane.YES_OPTION) {
+//                int tempRow = contacts_frame.getPanel().getContact_view().getTable().convertRowIndexToModel(contacts_frame.getPanel().getContact_view().getTable().getSelectedRow());
+                tableToFile(-1);
+                updateArrayAndTable();
+            }
+            JOptionPane.showMessageDialog(null, "Saved");
+
+        }
+    }
+
+    class ImportButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int returnVal = contacts_frame.getPanel().getContact_view().getImportChooser().showOpenDialog(null);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File f = contacts_frame.getPanel().getContact_view().getImportChooser().getSelectedFile();
+                String importDir = f.getAbsolutePath();
+                System.out.println(importDir);
+
+                String tempName;
+                contactsList.clear();
+                contacts_frame.getPanel().getContact_view().getModel().setRowCount(0);
+                try {
+                    FileReader fin;
+                    fin = new FileReader(importDir);
+                    Scanner scan = new Scanner(fin);
+                    while (scan.hasNext()) {
+
+                        tempName = scan.next();
+                        String[] InArray = tempName.split(",");
+
+                        contactsList.add(new Contacts(InArray[0], InArray[1], InArray[2]));
+
+                        contacts_frame.getPanel().getContact_view().getModel().addRow(new Object[]{InArray[0], InArray[1], InArray[2]});
+
+                    }
+                    fin.close();
+                    contacts_frame.getPanel().getContact_view().getTable().setModel(contacts_frame.getPanel().getContact_view().getTable().getModel());
+
+                } catch (FileNotFoundException ex) {
+
+                    JOptionPane.showMessageDialog(null, ex);
+                } catch (IOException ex) {
+                }
+
+            }
+        }
+    }
+
+    class ExportButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int returnVal = contacts_frame.getPanel().getContact_view().getExportChooser().showOpenDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File f = contacts_frame.getPanel().getContact_view().getExportChooser().getSelectedFile();
+                String exportDir = f.getAbsolutePath();
+                System.out.println(exportDir);
+                int confirm = -1;
+//                if (f.exists() && !f.isDirectory()) {
+                confirm = JOptionPane.showConfirmDialog(null, "This WILL overwrite a file if it already exist. \nDo You Wish To Continue?");
+//                }
+                if (confirm == JOptionPane.YES_OPTION) {
+                    FileWriter fout;
+                    try {
+                        fout = new FileWriter(String.format(exportDir + "/%s.csv", getCurrentUsersName()));
+                        for (int i = 0; i < contacts_frame.getPanel().getContact_view().getTable().getRowCount(); i++) {
+                            for (int j = 0; j < contacts_frame.getPanel().getContact_view().getTable().getColumnCount(); j++) {
+
+                                if (j == 2) {
+                                    fout.write(contacts_frame.getPanel().getContact_view().getTable().getValueAt(i, j) + "");
+                                } else {
+                                    fout.write(contacts_frame.getPanel().getContact_view().getTable().getValueAt(i, j) + ",");
+                                }
+
+                            }
+                            fout.write("\n");
+                        }
+                        fout.close();
+                        fout.flush();
+                    } catch (IOException ex) {
+                    }
+
+                }
+            }
+        }
+    }
+
     class AddButtonListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             FileWriter fout;
             try {
-                fout = new FileWriter(String.format("src/contacts/%s.csv",getCurrentUsersName()), true);
+                fout = new FileWriter(String.format("src/contacts/%s.csv", getCurrentUsersName()), true);
                 tempC = JOptionPane.showInputDialog(null, "Contact Name: ");
                 fout.write("\n" + tempC + ",");
                 tempC = JOptionPane.showInputDialog(null, "Phone Number: ");
@@ -79,7 +182,7 @@ public class ContactsController {
             updateArrayAndTable();
         }
     }
-    
+
     public void updateArrayAndTable() {
 
         String tempName;
@@ -87,10 +190,10 @@ public class ContactsController {
         contacts_frame.getPanel().getContact_view().getModel().setRowCount(0);
         try {
             FileReader fin;
-            fin = new FileReader(String.format("src/contacts/%s.csv",getCurrentUsersName()));
+            fin = new FileReader(String.format("src/contacts/%s.csv", getCurrentUsersName()));
             Scanner scan = new Scanner(fin);
             while (scan.hasNext()) {
-                
+
                 tempName = scan.next();
                 String[] InArray = tempName.split(",");
 
@@ -108,20 +211,19 @@ public class ContactsController {
         } catch (IOException ex) {
         }
     }
-    
-     public void tableToFile(int tempRow) {
+
+    public void tableToFile(int tempRow) {
         FileWriter fout;
         try {
-            fout = new FileWriter(String.format("src/contacts/%s.csv",getCurrentUsersName()));
+            fout = new FileWriter(String.format("src/contacts/%s.csv", getCurrentUsersName()));
             for (int i = 0; i < contacts_frame.getPanel().getContact_view().getTable().getRowCount(); i++) {
                 for (int j = 0; j < contacts_frame.getPanel().getContact_view().getTable().getColumnCount(); j++) {
 
                     if (i != tempRow) {
-                        if(j ==  2){
+                        if (j == 2) {
                             fout.write(contacts_frame.getPanel().getContact_view().getTable().getValueAt(i, j) + "");
-                            }
-                        else {
-                        fout.write(contacts_frame.getPanel().getContact_view().getTable().getValueAt(i, j) + ",");
+                        } else {
+                            fout.write(contacts_frame.getPanel().getContact_view().getTable().getValueAt(i, j) + ",");
                         }
                     }
                 }
@@ -133,9 +235,9 @@ public class ContactsController {
         }
 
     }
-     
-         class SearchDocumentListener implements DocumentListener {
-        
+
+    class SearchDocumentListener implements DocumentListener {
+
         @Override
         public void insertUpdate(DocumentEvent e
         ) {
@@ -146,7 +248,7 @@ public class ContactsController {
                 contacts_frame.getPanel().getContact_view().getRowSorter().setRowFilter(RowFilter.regexFilter("(?i)" + text));
             }
         }
-        
+
         @Override
         public void removeUpdate(DocumentEvent e
         ) {
@@ -157,7 +259,7 @@ public class ContactsController {
                 contacts_frame.getPanel().getContact_view().getRowSorter().setRowFilter(RowFilter.regexFilter("(?i)" + text));
             }
         }
-        
+
         @Override
         public void changedUpdate(DocumentEvent e) {
             JOptionPane.showMessageDialog(null, "Not supported yet.");
@@ -171,4 +273,5 @@ public class ContactsController {
     public void setCurrentUsersName(String currentUsersName) {
         this.currentUsersName = currentUsersName;
     }
+
 }
--- src/pkg311project/ContactsView.java
+++ src/pkg311project/ContactsView.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -16,6 +17,7 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
@@ -25,23 +27,31 @@ import javax.swing.table.TableRowSorter;
  * @author ajl5735
  */
 public class ContactsView extends JPanel {
-
-        Object[][] data = {{"", "", ""}, {"", "", ""}};
-        String[] columnName = {"Name", "Phone Number", "Extra"};
-        DefaultTableModel model = new DefaultTableModel(data, columnName);
-        JTable table = new JTable(model);
-        private TableRowSorter<TableModel> rowSorter
+    
+    Object[][] data = {{"", "", ""}, {"", "", ""}};
+    String[] columnName = {"Name", "Phone Number", "Extra"};
+    DefaultTableModel model = new DefaultTableModel(data, columnName);
+    JTable table = new JTable(model);
+    private TableRowSorter<TableModel> rowSorter
             = new TableRowSorter<>(table.getModel());
-        JScrollPane scroll;
-
-        JLabel hi = new JLabel("hi");
-        JLabel filterLabel;
-        JButton save = new JButton("Save Edits");
-        JButton add = new JButton("Add Contacts");
-        JButton delete = new JButton("Delete Button");
-        JPanel bottomPanel;
-        JTextArea filter;
-
+    JScrollPane scroll;
+    
+    JLabel hi = new JLabel("hi");
+    JLabel filterLabel;
+    JButton save = new JButton("Save Edits");
+    JButton add = new JButton("Add Contacts");
+    JButton delete = new JButton("Delete Button");
+    JButton export = new JButton("Excport");
+    JPanel bottomPanel;
+    JTextArea filter;
+    JButton importButton = new JButton("Import");
+    
+    JFileChooser importChooser = new JFileChooser();
+    FileNameExtensionFilter importFileFilter = new FileNameExtensionFilter(
+            "CSV files (*csv)", "csv");
+    
+    JFileChooser exportChooser = new JFileChooser();
+    
     public ContactsView() {
         filterLabel = new JLabel("Search Filter: ", SwingConstants.RIGHT);
         
@@ -49,9 +59,9 @@ public class ContactsView extends JPanel {
         table.setRowSorter(rowSorter);
         table.setRowSelectionAllowed(true);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-       scroll = new JScrollPane(table);
+        scroll = new JScrollPane(table);
         bottomPanel = new JPanel();
-         bottomPanel.setLayout(new GridLayout(1,4));
+        bottomPanel.setLayout(new GridLayout(1, 7));
         
         filter = new JTextArea();
         filter.setOpaque(true);
@@ -59,32 +69,95 @@ public class ContactsView extends JPanel {
         bottomPanel.add(save);
         bottomPanel.add(add);
         bottomPanel.add(delete);
+        bottomPanel.add(importButton);
+        bottomPanel.add(export);
         bottomPanel.add(filterLabel);
         bottomPanel.add(filter);
         add(scroll, BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
+        
+        importChooser.setFileFilter(importFileFilter);
+        importChooser.setDialogTitle("Import CSV");
+        exportChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        exportChooser.setDialogTitle("Export CSV");
+        
+    }
+    
+    public JButton getImportButton() {
+        return importButton;
+    }
+    
+    public void setImportButton(JButton importButton) {
+        this.importButton = importButton;
+    }
+    
+    public JFileChooser getImportChooser() {
+        return importChooser;
+    }
+    
+    public void setImportChooser(JFileChooser importChooser) {
+        this.importChooser = importChooser;
+    }
+    
+    public FileNameExtensionFilter getImportFileFilter() {
+        return importFileFilter;
+    }
+    
+    public void setImportFileFilter(FileNameExtensionFilter importFileFilter) {
+        this.importFileFilter = importFileFilter;
+    }
+    
+    public JFileChooser getExportChooser() {
+        return exportChooser;
+    }
+    
+    public void setExportChooser(JFileChooser exportChooser) {
+        this.exportChooser = exportChooser;
+    }
+    
+    public JButton getSave() {
+        return save;
     }
 
+    public JButton getExport() {
+        return export;
+    }
+
+    public void setExport(JButton export) {
+        this.export = export;
+    }
+    
+    public void setSave(JButton save) {
+        this.save = save;
+    }
+
+//    public FileNameExtensionFilter getFileFilter() {
+//        return fileFilter;
+//    }
+//
+//    public void setFileFilter(FileNameExtensionFilter fileFilter) {
+//        this.fileFilter = fileFilter;
+//    }
     public JButton getDelete() {
         return delete;
     }
-
+    
     public void setDelete(JButton delete) {
         this.delete = delete;
     }
-
+    
     public TableRowSorter<TableModel> getRowSorter() {
         return rowSorter;
     }
-
+    
     public void setRowSorter(TableRowSorter<TableModel> rowSorter) {
         this.rowSorter = rowSorter;
     }
-
+    
     public JButton getAdd() {
         return add;
     }
-
+    
     public void setAdd(JButton add) {
         this.add = add;
     }
@@ -92,7 +165,7 @@ public class ContactsView extends JPanel {
     public JTable getTable() {
         return table;
     }
-
+    
     public void setTable(JTable table) {
         this.table = table;
     }
@@ -100,15 +173,15 @@ public class ContactsView extends JPanel {
     public DefaultTableModel getModel() {
         return model;
     }
-
+    
     public JTextArea getFilter() {
         return filter;
     }
-
+    
     public void setFilter(JTextArea filter) {
         this.filter = filter;
     }
-
+    
     public void setModel(DefaultTableModel model) {
         this.model = model;
     }
